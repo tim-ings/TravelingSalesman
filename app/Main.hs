445480757b183bc9ba0ecf47f134c6fef20542d4
Tim Ings <tim@tim-ings.com>
2020-08-17 18:47:21 +0800

    Adds show and read functions to parse input data

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
-import Lib
+type City = (Float, Float)
 
 main :: IO ()
-main = someFunc
+main = interact handle
+
+handle :: String -> String
+handle = showCities . readCities
+
+readCities :: String -> [City]
+readCities = (map readCity) . lines
+
+readCity :: String -> City
+readCity coords = (x, y)
+    where ws = words coords
+          x = read $ head ws
+          y = read $ head $ tail ws
+
+showCities :: [City] -> String
+showCities = unlines . (map showCity)
+
+showCity :: City -> String
+showCity city = show (fst city) ++ show (snd city)
